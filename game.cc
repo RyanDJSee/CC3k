112b34730a3bde3e64f84eb59da6040709607dcc
j55liang <mushroomyuan@gmail.com>
2017-03-30 19:42:00 -0400

    3.30 v5 main and game

--- game.cc
+++ game.cc
@@ -1,25 +1,50 @@
-#ifndef GAME_H
-#define GAME_H
-
 #include <string>
+#include <memory>
+#include "RESTART.h"
+#include "game.h"
+#include "Shade.h"
 using namespace std;
 
+Game::Game(): currFloor(nullptr), currFloornum(1), pc(nullptr) {}
 
-Class Floor;
-Class Cell;
 
-class Game {
-  Floor *currFloor;
-  int currFloornum;
-  PC *pc; //only uses getInfo
+void Game::GameInit(string character, string filename="") {//needs new character, start from floor 1
+  if (character=="s") {
+    pc = make_shared<Shade{-1, -1, -1}>;
+  } else if (character=="d") {
+    pc = make_shared<Drow{-1, -1, -1}>;
+  } else if (character=="v") {
+    pc = make_shared<Vampire{-1, -1, -1}>;
+  } else if (character=="g") {
+    pc = make_shared<Goblin{-1, -1, -1}>;
+  } else if (character=="t") {
+    pc = make_shared<Troll{-1, -1, -1}>;
+  }
+  currFloor = make_shared<Floor(1, pc)>;
+  currFloor->FloorInit(filename);
+}
 
-public:
-  Game();
-  void GameInit(string char);//needs new character, start from floor 1
-  void Restart();
-  void MoveChar(string dir);
-  void isWon();
-  ~Game();
-};
 
-#endif
+  void UsePotion(string dir){
+    currFloor->UsePotion(dir);
+  }
+
+
+  void Attack(string dir){
+    currFloor->Attack(dir);
+  }
+
+
+  void EnemySwitch(){
+    currFloor->EnemySwitch();
+  }
+
+
+  void Restart(){
+    throw RESTART;
+  }
+
+
+  void MoveChar(string dir){
+    currFloor->Floormove(pc, dir);
+  }
