3a022703479e142ac0dab87197954c118029593b
j55liang <mushroomyuan@gmail.com>
2017-04-01 20:14:08 -0400

    4.1 v18 subject

--- subject.cc
+++ subject.cc
@@ -20,25 +20,25 @@ void Subject::dettachall(){
 void Subject::notifyObservers(SubType t) {
   //go through all observers and notify ones with type t
   for (auto ob : observers) {
-    if (ob->subType()==t) {
+    if (ob->subtype()==t) {
       ob->notify(*this);
     }
   }
 }
 
 
-virtual void Subject::attackedBy(Human &hu){}
-virtual void Subject::attackedBy(Dwarf &dw){}
-virtual void Subject::attackedBy(Halfling &half){}
-virtual void Subject::attackedBy(Elf &elf){}
-virtual void Subject::attackedBy(Orc &orc){}
-virtual void Subject::attackedBy(Merchant &mer){}
-virtual void Subject::attackedBy(Dragon &dra){}
-virtual void Subject::attackedBy(Shade& shade){}
-virtual void Subject::attackedBy(Goblin& goblin) {}
-virtual void Subject::attackedBy(Vampire& vampire) {}
-virtual void Subject::attackedBy(Troll& troll){}
-virtual void Subject::attackedBy(Drow& drow){}
+void Subject::attackedBy(Human &hu){}
+void Subject::attackedBy(Dwarf &dw){}
+void Subject::attackedBy(Halfling &half){}
+void Subject::attackedBy(Elf &elf){}
+void Subject::attackedBy(Orc &orc){}
+void Subject::attackedBy(Merchant &mer){}
+void Subject::attackedBy(Dragon &dra){}
+void Subject::attackedBy(Shade& shade){}
+void Subject::attackedBy(Goblin& goblin) {}
+void Subject::attackedBy(Vampire& vampire) {}
+void Subject::attackedBy(Troll& troll){}
+void Subject::attackedBy(Drow& drow){}
 
 
 Subject:: ~Subject() {}
