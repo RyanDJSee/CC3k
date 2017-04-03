c8bc41504a23fd084f47caa1f51970c480ff966d
anncai <hellocaicaicai@126.com>
2017-04-02 20:57:44 -0400

    Delete merchant.d

--- merchant.d
+++
@@ -1,3 +0,0 @@
-merchant.o: merchant.cc merchant.h enemy.h character.h item.h cell.h \
- steptype.h subtype.h subject.h observer.h info.h shade.h pc.h goblin.h \
- vampire.h drow.h troll.h rh.h potion.h
